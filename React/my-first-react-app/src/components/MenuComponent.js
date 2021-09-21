a36bc0071c094447dbbf3198c60c3db36da74508
MissTecHack <woman.in.me77@gmail.com>
2021-09-22 00:33:42 +0530

    Added Cards for Event Handling

--- React/my-first-react-app/src/components/MenuComponent.js
+++ React/my-first-react-app/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Media} from 'reactstrap';
+import { Card, CardFooter, CardImg, Col, Media, Row } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
@@ -17,6 +17,20 @@ class Menu extends Component {
                         <Media body className="col-7 ml-5">
                             <Media heading>{sp.name}</Media>
                             <p>{sp.description}</p>
+                            <Row>
+                                {
+                                    sp.types.map((type) => {
+                                        return(
+                                            <Col sm="3">
+                                                <Card key={type.id}>
+                                                    <CardImg src={type.image} alt={type.name} />
+                                                    <CardFooter>{type.name}</CardFooter>
+                                                </Card>
+                                            </Col>
+                                        );
+                                    })
+                                }
+                            </Row>
                         </Media>
                     </Media>
                 </div>
