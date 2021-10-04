dde22b0a57652147523adcda8b8ca1fb7c49a093
MissTecHack <woman.in.me77@gmail.com>
2021-10-04 17:53:09 +0530

    Store name updated in App.js

--- React/my-first-react-app/src/App.js
+++ React/my-first-react-app/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Main from './components/MainComponent';
 import {BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+import { ConfigureNewStore } from './redux/configureNewStore';
 
-const store = ConfigureStore();
+const store = ConfigureNewStore();
 
 class App extends Component {
 
